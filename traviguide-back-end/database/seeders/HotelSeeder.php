e5e07a8a6f12375caac5563d8a923630f0ec91df
Ghina Alrefai <ghina.alrefai12@gmail.com>
2025-03-20 17:09:00 +0300

    Created HotelsSeeder and added initial hotel data

--- traviguide-back-end/database/seeders/HotelSeeder.php
+++ traviguide-back-end/database/seeders/HotelSeeder.php
@@ -2,14 +2,22 @@
 
 namespace Database\Seeders;
 
-use App\Models\Hotel;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class HotelSeeder extends Seeder
 {
     public function run()
     {
-        Hotel::factory()->count(10)->create();
+        // قراءة محتوى ملف JSON
+        $json = File::get(database_path('seeders/data/hotels.json'));
+
+        // تحويل البيانات إلى مصفوفة
+        $data = json_decode($json, true);
+
+        // إدخال البيانات في قاعدة البيانات
+        DB::table('hotels')->insert($data);
     }
 }
+
